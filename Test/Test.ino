9e60aa0a1c16fe2baa9fe309768faa8a86404dd3
Rudolf <rudolf.adam@outlook.de>
2023-02-01 12:23:58 +0100

    main

---
+++ Test/Test.ino
@@ -0,0 +1,8 @@
+[env:nodemcuv2]
+platform = espressif8266
+board = nodemcuv2
+framework = arduino
+upload_port = /dev/ttyUSB*
+lib_deps = 
+    adafruit/Adafruit Unified Sensor @ ^1.1.7
+    adafruit/DHT sensor library @ ^1.4.4
